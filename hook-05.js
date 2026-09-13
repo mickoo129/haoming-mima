73f47e98f8a73e29eaccbd40da229c3a472e2023
mickoo129 <michaelsckoo@gmail.com>
2026-09-14 00:56:14 +0800

    add hook-05.js: id/address/account/other keep 0 and 5 as book fuwei-follow

---
+++ hook-05.js
@@ -0,0 +1,28 @@
+(function(){
+ var old=window.buildPairs||buildPairs;
+ function keep05(digits,kind){
+  var out=[],i,pair,key,field,note;
+  for(i=0;i<digits.length-1;i++){
+   pair=digits.charAt(i)+digits.charAt(i+1);
+   key=PAIR_MAP[pair];
+   field=key||'fuwei';
+   note='';
+   if(!key){
+    if(pair.indexOf('0')>=0||pair.indexOf('5')>=0) note='書：0／5自己唔成星，當伏';
+    else note='書：非八星組合當伏';
+   }
+   if(field==='fuwei'&&out.length){
+    field=out[out.length-1].field;
+    note+=(note?'；':'')+'跟前「'+FIELDS[field].name+'」';
+   }
+   out.push({pair:pair,display:pair,field:field,note:note});
+  }
+  return out;
+ }
+ function wrapped(digits,kind){
+  if(kind==='id'||kind==='address'||kind==='account'||kind==='other') return keep05(digits,kind);
+  return old(digits,kind);
+ }
+ window.buildPairs=wrapped;
+ buildPairs=wrapped;
+})();
