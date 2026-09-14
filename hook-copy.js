5e53049cdffe600afe2a0d59c738d2fae7f86d8c
mickoo129 <michaelsckoo@gmail.com>
2026-09-14 12:15:57 +0800

    unify all leftover 教材/高級課程/課堂車牌筆記 headings to 課堂/書/筆記

--- hook-copy.js
+++ hook-copy.js
@@ -1,25 +1,47 @@
 (function(){
+ function relabel(s){
+  if(!s)return s;
+  return s
+   .replace(/課堂車牌筆記/g,'筆記')
+   .replace(/課堂睇號筆記/g,'筆記')
+   .replace(/教材筆記/g,'筆記')
+   .replace(/高級課程/g,'筆記')
+   .replace(/教材大凶：/g,'課堂：')
+   .replace(/教材適合的行業/g,'課堂：適合的行業')
+   .replace(/百科主文跟教材/g,'百科主文跟課堂')
+   .replace(/手機教材分類提示/g,'課堂手機分類提示')
+   .replace(/教材禁號/g,'課堂禁號')
+   .replace(/教材例子/g,'筆記與課堂例子')
+   .replace(/教材：/g,'課堂：');
+ }
+ if(typeof KIND_INFO!=='undefined'){
+  Object.keys(KIND_INFO).forEach(function(k){
+   if(KIND_INFO[k]&&KIND_INFO[k].text) KIND_INFO[k].text=relabel(KIND_INFO[k].text);
+  });
+ }
  var oldExtra=window.extraCourseNotes;
  extraCourseNotes=function(digits,pairs,kind){
   if(kind==='id')return '';
   var html=(typeof oldExtra==='function')?oldExtra(digits,pairs,kind):'';
-  return html.replace(/教材筆記/g,'筆記').replace(/高級課程/g,'筆記');
+  return relabel(html);
  };
  window.extraCourseNotes=extraCourseNotes;
  function cleanBoxes(){
   var kind=window.currentKind||(typeof currentKind!=='undefined'?currentKind:'phone');
-  ['kindReadBox','details','fixBox','storyBox','roleBox'].forEach(function(id){
+  ['kindReadBox','details','fixBox','storyBox','roleBox','wikiGrid'].forEach(function(id){
    var el=document.getElementById(id);
    if(!el||!el.innerHTML)return;
-   el.innerHTML=el.innerHTML.replace(/教材筆記/g,'筆記').replace(/高級課程/g,'筆記').replace(/教材：/g,'');
+   el.innerHTML=relabel(el.innerHTML);
   });
+  var hint=document.getElementById('kindHint');
+  if(hint)hint.innerHTML=relabel(hint.innerHTML);
   var showLeg=(kind==='phone'||kind==='plate'||kind==='other');
   var box=document.getElementById('kindReadBox');
   if(box){
    var leg=box.querySelector('.hl-legend');
    if(!showLeg&&leg)leg.parentNode.removeChild(leg);
    if(showLeg&&box.innerHTML&&!box.querySelector('.hl-legend')){
-    box.innerHTML+='<p class="hl-legend"><i><span class="num-in">黃底</span> 呢組真係有呢個號</i><i><span class="num-def">綠邊</span> 星點組成（規則，唔等於你全部都有）</i><i><span class="num-ex">灰虛線</span> 教材例子</i></p>';
+    box.innerHTML+='<p class="hl-legend"><i><span class="num-in">黃底</span> 呢組真係有呢個號</i><i><span class="num-def">綠邊</span> 星點組成（規則，唔等於你全部都有）</i><i><span class="num-ex">灰虛線</span> 課堂／筆記例子</i></p>';
    }
   }
  }
@@ -35,6 +57,7 @@
   wrapped.__layWrapped=impl.__layWrapped;
   window.analyze=wrapped;
   window.hmAnalyze=function(){window.analyze();return false;};
+  setTimeout(cleanBoxes,0);
  }
  if(document.readyState==='loading')document.addEventListener('DOMContentLoaded',function(){setTimeout(wrap,0);});
  else setTimeout(wrap,0);
