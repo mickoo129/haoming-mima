a9e1dd690a30799b7ec9a9e0859e1bdbdbb3ff8f
mickoo129 <michaelsckoo@gmail.com>
2026-09-20 10:07:55 +0800

    Bump ID year-note hook cache

--- hook-05.js
+++ hook-05.js
@@ -95,7 +95,7 @@
  window.buildPairs=wrapped;
  buildPairs=wrapped;
  var ys=document.createElement('script');
- ys.src='./hook-id-year.js?v=20260920b';
+ ys.src='./hook-id-year.js?v=20260920c';
  if(document.body)document.body.appendChild(ys);
  else document.addEventListener('DOMContentLoaded',function(){document.body.appendChild(ys);});
 })();
